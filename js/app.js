75ba422106f350e0c0a95629f8612d088555495d
Alexey Drozdov <alexey.drozdov@gmail.com>
2015-08-28 17:40:11 +0300

    First pages commit

---
+++ js/app.js
@@ -0,0 +1,384 @@
+// Enemies our player must avoid
+var Enemy = function(x, y) {
+    // Variables applied to each of our instances go here,
+    // we've provided one for you to get started
+
+    // The image/sprite for our enemies, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/enemy-bug.png';
+    // Initial position of enemy
+    this.x = x;
+    this.y = y;
+    // Initial speed of enemy (random number from the interval)
+    this.speed = getRandomInt(50,400);
+};
+
+// Update the enemy's position, required method for game
+// Parameter: dt, a time delta between ticks
+Enemy.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    this.x += this.speed*dt;
+    // Set new speed and new x position if enemy goes out from the screen
+    this.speed = this.x > 700 ? getRandomInt(50,400) : this.speed;
+    this.x = this.x > 700 ? getRandomInt(-100,-200) : this.x;
+};
+
+// Draw the enemy on the screen, required method for game
+Enemy.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Our player class
+var Player = function(x, y) {
+    // Initial position of player
+    this.x = x;
+    this.y = y;
+    // Initial player image
+    this.sprite = 'images/char-boy.png';
+    // Initial number of player life
+    this.life = 5;
+    // INitial state for flag if player win or game still continues
+    this.win = false;
+};
+
+// Update the players's position
+// Actually function is not required as all movements are handled by handleInput function
+//Player.prototype.update = function() {
+//};
+
+// Draw the player on the screen, required method for game
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Handle input for player
+// All movements and character image changes are implemented in this function
+Player.prototype.handleInput = function(input) {
+    // Skip if input is incorrect or player is dead or player win
+    if(!input || this.life === 0 || this.win) return;
+
+    // Data structures for movement and player images
+    var moveX = {
+        'left': -100,
+        'right': 100
+    };
+    var moveY = {
+        'up': -85,
+        'down': 85
+    };
+    var images = {
+        '1': 'images/char-boy.png',
+        '2': 'images/char-cat-girl.png',
+        '3': 'images/char-horn-girl.png',
+        '4': 'images/char-pink-girl.png',
+        '5': 'images/char-princess-girl.png',
+    };
+
+    // Move player if some arrow is pressed
+    if (input == 'up' || input == 'down' || input == 'left' || input == 'right') {
+        // Move x direction
+        // ... || 0 operator is used if we actually move y direction
+        this.x += moveX[input] || 0;
+        // Move y direction
+        // ... || 0 operator is used if we actually move x direction
+        this.y += moveY[input] || 0;
+        // Set win if we reach water
+        this.win = this.y < 0 ? true : false;
+        // Does not allow player to move out of screen
+        this.x = this.x < 0 ? 0 : this.x;
+        this.y = this.y < 0 ? 563 : this.y;
+        this.x = this.x > 600 ? 600 : this.x;
+        this.y = this.y > 563 ? 563 : this.y;
+    // Change player image if number from 1 to 5 is pressed
+    } else if (input == '1' || input == '2' || input == '3' || input == '4' || input == '5') {
+        this.sprite = images[input];
+    }
+};
+
+// Class for item (resource) shown on the field
+var Item = function(x, y) {
+    // Data structure of available items
+    var itemImages = [
+            'images/gem-blue.png',
+            'images/gem-orange.png',
+            'images/gem-green.png',
+            'images/Heart.png',
+            'images/Key.png',
+            'images/Rock.png',
+            'images/Selector.png',
+            'images/Star.png'
+           ];
+
+    // Data structure of available items and their types
+    var itemImagesType = {
+            'images/gem-blue.png': 'blueGem',
+            'images/gem-orange.png': 'orangeGem',
+            'images/gem-green.png': 'greenGem',
+            'images/Heart.png': 'heart',
+            'images/Key.png': 'key',
+            'images/Rock.png': 'rock',
+            'images/Selector.png': 'selector',
+            'images/Star.png': 'star'
+           };
+
+    // Set initial position of item
+    this.x = x;
+    this.y = y;
+    // Set initial image of item
+    // Random value from appropriate data structure
+    this.sprite = itemImages[getRandomInt(0,7)];
+    // Set type of item in accordance with its image set before
+    this.type = itemImagesType[this.sprite];
+    // Flag was item collected by player or not
+    this.collide = false;
+    // Area in the field to display item
+    // The field is devided by for areas:
+    // - upper left corner - 3x3 area - pos = 1
+    // - upper right corner - 4x3 area - pos = 2
+    // - bottom left corner - 3x3 area - pos = 3
+    // - bottom right corner - 4x3 area - pos = 4
+    // Inital pos is 0. It is set immidiatly after creation of item
+    this.pos = 0;
+    // Timer that is set in not 0 value if item was collected
+    // It represent how much ticks new item should not appear on field after collecting
+    this.timer = 0;
+
+};
+
+// Random choose of items image
+Item.prototype.newSprite = function() {
+    // Data structure from which item is choosen
+    var itemImages = [
+            'images/gem-blue.png',
+            'images/gem-orange.png',
+            'images/gem-green.png',
+            'images/Heart.png',
+            'images/Key.png',
+            'images/Rock.png',
+            'images/Selector.png',
+            'images/Star.png'
+           ];
+    // Random set of item image
+    this.sprite = itemImages[getRandomInt(0,7)];
+};
+
+// Setting of item type in accordance to its image
+Item.prototype.newType = function() {
+    // Data structure from which type is choosen
+    var itemImagesType = {
+            'images/gem-blue.png': 'blueGem',
+            'images/gem-orange.png': 'orangeGem',
+            'images/gem-green.png': 'greenGem',
+            'images/Heart.png': 'heart',
+            'images/Key.png': 'key',
+            'images/Rock.png': 'rock',
+            'images/Selector.png': 'selector',
+            'images/Star.png': 'star'
+           };
+    // Set items type
+    this.type = itemImagesType[this.sprite];
+};
+
+// Update item image, type and position if it was collected
+Item.prototype.update = function(dt) {
+    // Perform only if item was collected (collide with player)
+    if (this.collide) {
+        // Set number of ticks after which new item will be shown
+        console.log(dt);
+        this.timer = 100;
+        // Set flag that new item is not collected yet
+        this.collide = false;
+        // Set new image, appropriate type and random position in appropriate area
+        if (this.pos == 1) {
+            this.newSprite();
+            this.newType();
+            this.x = getRandomInt(0,2)*100;
+            this.y = 60+getRandomInt(0,2)*83;
+        } else if (this.pos == 2) {
+            this.newSprite();
+            this.newType();
+            this.x = getRandomInt(3,6)*100;
+            this.y = 60+getRandomInt(0,2)*83;
+        } else if (this.pos == 3) {
+            this.newSprite();
+            this.newType();
+            this.x = getRandomInt(0,2)*100;
+            this.y = 60+getRandomInt(3,5)*83;
+        } else if (this.pos == 4) {
+            this.newSprite();
+            this.newType();
+            this.x = getRandomInt(3,6)*100;
+            this.y = 60+getRandomInt(3,5)*83;
+        }
+    }
+    // If timer is not 0 - tick
+    if (this.timer) {
+        this.timer -= 1;
+    }
+};
+
+// Draw the item on the screen
+Item.prototype.render = function() {
+    // If timer is 0 - draw
+    if (!this.timer) {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+};
+
+// Class to draw score on all related inforamtion
+var Score = function() {
+    // Initial values for:
+    // - collected items (zero at the start)
+    // - number of times water was reached (zero at the start)
+    // - players life (5 at the start)
+    this.score = {
+        'blueGem' : 0,
+        'orangeGem' : 0,
+        'greenGem' : 0,
+        'key' : 0,
+        'selector' : 0,
+        'star' : 0,
+        'waterReached' : 0,
+        'life' : 5
+    };
+    // Flag if player win or not
+    this.win = false;
+    // Flag if player lose (life is zero) or not
+    this.lose = false;
+};
+
+// Add score to item with appropriate type if it was collected
+Score.prototype.addScore = function(type) {
+    this.score[type] += 1;
+};
+
+// Clear all score area. This function is invoked each tick
+// It is analog of update functions in other classes
+// It is called so for more clearness of its usage
+Score.prototype.clearScore = function() {
+    ctx.clearRect(20, 750, 750, 250);
+    ctx.clearRect(5, 20, 750, 30);
+};
+
+// Draw all score area
+Score.prototype.render = function() {
+    // Draw all items if player is not win and not lose (if game continues)
+    if(!this.lose && !this.win) {
+
+        // Draw tip at the top of the field
+        ctx.lineWidth = 1;
+        ctx.font = "24px Arial";
+        ctx.fillText("To Win: collect 3 resources of each type and reach water 3 times",5,40);
+
+        // Draw options to choose character
+        // Charaters could be choosen by appropriate number pressing
+        // So we draw here a number and an image of appropriate character after it
+        ctx.font = "30px Arial";
+        ctx.fillText("Choose character: ",20,790);
+        ctx.font = "24px Arial";
+        ctx.fillText("1-",310,790);
+        ctx.drawImage(Resources.get('images/char-boy-small.png'), 330, 730);
+        ctx.fillText("2-",390,790);
+        ctx.drawImage(Resources.get('images/char-cat-girl-small.png'), 410, 730);
+        ctx.fillText("3-",470,790);
+        ctx.drawImage(Resources.get('images/char-horn-girl-small.png'), 490, 730);
+        ctx.fillText("4-",550,790);
+        ctx.drawImage(Resources.get('images/char-pink-girl-small.png'), 570, 730);
+        ctx.fillText("5-",630,790);
+        ctx.drawImage(Resources.get('images/char-princess-girl-small.png'), 650, 730);
+
+        // Draw all available types of items in the game
+        ctx.drawImage(Resources.get('images/gem-blue-small.png'), 20, 860);
+        ctx.drawImage(Resources.get('images/gem-orange-small.png'), 140, 860);
+        ctx.drawImage(Resources.get('images/gem-green-small.png'), 260, 860);
+        ctx.drawImage(Resources.get('images/Star-small.png'), 380, 860);
+        ctx.drawImage(Resources.get('images/Selector-small.png'), 500, 855);
+        ctx.drawImage(Resources.get('images/Key-small.png'), 620, 860);
+        // Draw number of collected items for each type
+        ctx.fillText(this.score.blueGem,37,965);
+        ctx.fillText(this.score.orangeGem,157,965);
+        ctx.fillText(this.score.greenGem,277,965);
+        ctx.fillText(this.score.star,397,965);
+        ctx.fillText(this.score.selector,517,965);
+        ctx.fillText(this.score.key,637,965);
+
+        // Draw number of times water was reached
+        ctx.font = "30px Arial";
+        ctx.fillText("Water reached: ",20,850);
+        ctx.fillText(this.score.waterReached,230,850);
+
+        // Draw number of player life
+        ctx.fillText("Life: ",320,850);
+        for (var i = 0; i < this.score.life; i++) {
+            ctx.drawImage(Resources.get('images/Heart-small.png'), 400 + i*50, 800);
+        }
+    // Draw congrutalaion message if player win
+    } else if (this.win) {
+        ctx.lineWidth = 1;
+        ctx.font = "24px Arial";
+        ctx.fillText("F5 to start new game",250,40);
+        ctx.font = "80px Arial";
+        ctx.fillText("Congratulations!",60,850);
+        ctx.fillText("You Win!",200,950);
+    // Draw message if player lose
+    } else if (this.lose) {
+        ctx.lineWidth = 1;
+        ctx.font = "24px Arial";
+        ctx.fillText("F5 to start new game",250,40);
+        ctx.font = "120px Arial";
+        ctx.fillText("Game Over!",20,850);
+    }
+};
+
+// Create the player object in a variable called player
+var player = new Player(getRandomInt(0,6)*100, 563);
+
+// Create all item objects in an array called allItems
+var allItems = [new Item(getRandomInt(0,2)*100, 60+getRandomInt(0,2)*83),
+                new Item(getRandomInt(3,6)*100, 60+getRandomInt(0,2)*83),
+                new Item(getRandomInt(0,2)*100, 60+getRandomInt(3,5)*83),
+                new Item(getRandomInt(3,6)*100, 60+getRandomInt(3,5)*83)];
+
+// Set items pos parameter to show at which area of screen it is shown
+for(var i=0, len = allItems.length; i < len; i++) {
+    allItems[i].pos = i+1;
+}
+
+// Create all enemy objects in an array called allEnemies
+var allEnemies = [new Enemy(0, 60),
+                  new Enemy(0, 143),
+                  new Enemy(0, 226),
+                  new Enemy(0, 309),
+                  new Enemy(0, 392),
+                  new Enemy(0, 475)];
+
+// Create new score for the game
+var score = new Score();
+
+
+// This listens for key presses and sends the keys to your
+// Player.handleInput() method.
+document.addEventListener('keyup', function(e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down',
+        49: '1',
+        50: '2',
+        51: '3',
+        52: '4',
+        53: '5'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+
+
+// Returns a random integer between min (inclusive) and max (inclusive)
+// Using Math.round() will give you a non-uniform distribution!
+ function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
---
+++ js/engine.js
@@ -0,0 +1,332 @@
+/* Engine.js
+ * This file provides the game loop functionality (update entities and render),
+ * draws the initial game board on the screen, and then calls the update and
+ * render methods on your player and enemy objects (defined in your app.js).
+ *
+ * A game engine works by drawing the entire game screen over and over, kind of
+ * like a flipbook you may have created as a kid. When your player moves across
+ * the screen, it may look like just that image/character is moving or being
+ * drawn but that is not the case. What's really happening is the entire "scene"
+ * is being drawn over and over, presenting the illusion of animation.
+ *
+ * This engine is available globally via the Engine variable and it also makes
+ * the canvas' context (ctx) object globally available to make writing app.js
+ * a little simpler to work with.
+ */
+
+var Engine = (function(global) {
+    /* Predefine the variables we'll be using within this scope,
+     * create the canvas element, grab the 2D context for that canvas
+     * set the canvas elements height/width and add it to the DOM.
+     */
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        lastTime;
+
+    canvas.width = 707;
+    canvas.height = 970;
+    doc.body.appendChild(canvas);
+
+
+    /* This function serves as the kickoff point for the game loop itself
+     * and handles properly calling the update and render methods.
+     */
+    function main() {
+        /* Get our time delta information which is required if your game
+         * requires smooth animation. Because everyone's computer processes
+         * instructions at different speeds we need a constant value that
+         * would be the same for everyone (regardless of how fast their
+         * computer is) - hurray time!
+         */
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+
+        /* Call our update/render functions, pass along the time delta to
+         * our update function since it may be used for smooth animation.
+         */
+        update(dt);
+        render();
+
+        /* Set our lastTime variable which is used to determine the time delta
+         * for the next time this function is called.
+         */
+        lastTime = now;
+
+        /* Use the browser's requestAnimationFrame function to call this
+         * function again as soon as the browser is able to draw another frame.
+         */
+        win.requestAnimationFrame(main);
+    }
+
+    /* This function does some initial setup that should only occur once,
+     * particularly setting the lastTime variable that is required for the
+     * game loop.
+     */
+    function init() {
+        reset();
+        lastTime = Date.now();
+        main();
+    }
+
+    /* This function is called by main (our game loop) and itself calls all
+     * of the functions which may need to update entity's data. Based on how
+     * you implement your collision detection (when two entities occupy the
+     * same space, for instance when your character should die), you may find
+     * the need to add an additional function call here. For now, we've left
+     * it commented out - you may or may not want to implement this
+     * functionality this way (you could just implement collision detection
+     * on the entities themselves within your app.js file).
+     */
+    function update(dt) {
+        /* Update entities, collisions and win state anly if game continues
+         * If player lose or win elements in the game should not be updated
+         */
+        if(!score.lose && !score.win) {
+            updateEntities(dt);
+            checkCollisions();
+            Win();
+        }
+        score.clearScore();
+    }
+
+    /* This is called by the update function  and loops through all of the
+     * objects within your allEnemies and allItems array as defined in app.js
+     * and calls their update() methods. It will then call the update function
+     * for your player object. These update methods should focus purely on
+     * updating the data/properties related to  the object. Do your drawing in
+     * your render methods.
+     */
+    function updateEntities(dt) {
+        allEnemies.forEach(function(enemy) {
+            enemy.update(dt);
+        });
+        allItems.forEach(function(item) {
+            item.update(dt);
+        });
+        //player.update();
+    }
+
+    /* This function initially draws the "game level", it will then call
+     * the renderEntities function. Remember, this function is called every
+     * game tick (or loop of the game engine) because that's how games work -
+     * they are flipbooks creating the illusion of animation but in reality
+     * they are just drawing the entire screen over and over.
+     */
+    function render() {
+        /* This array holds the relative URL to the image used
+         * for that particular row of the game level.
+         */
+        var rowImages = [
+                'images/water-block.png',   // Top row is water
+                'images/stone-block.png',   // Row 1 of 6 of stone
+                'images/stone-block.png',   // Row 2 of 6 of stone
+                'images/stone-block.png',   // Row 3 of 6 of stone
+                'images/stone-block.png',   // Row 4 of 6 of stone
+                'images/stone-block.png',   // Row 5 of 6 of stone
+                'images/stone-block.png',   // Row 6 of 6 of stone
+                'images/grass-block.png'    // Bottom ro is grass
+            ],
+            numRows = 8,
+            numCols = 7,
+            row, col;
+
+        /* Render score area
+         */
+        score.render();
+
+        /* Loop through the number of rows and columns we've defined above
+         * and, using the rowImages array, draw the correct image for that
+         * portion of the "grid"
+         */
+        for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+                /* The drawImage function of the canvas' context element
+                 * requires 3 parameters: the image to draw, the x coordinate
+                 * to start drawing and the y coordinate to start drawing.
+                 * We're using our Resources helpers to refer to our images
+                 * so that we get the benefits of caching these images, since
+                 * we're using them over and over.
+                 */
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
+        }
+
+        /* Render entities
+         */
+        renderEntities();
+    }
+
+    /* This function is called by the render function and is called on each game
+     * tick. It's purpose is to then call the render functions you have defined
+     * on your enemy and player entities within app.js
+     */
+    function renderEntities() {
+        /* Loop through all of the objects within the allEnemies array and call
+         * the render function you have defined.
+         */
+        allItems.forEach(function(item) {
+            item.render();
+        });
+
+        allEnemies.forEach(function(enemy) {
+            enemy.render();
+        });
+
+        player.render();
+    }
+
+    /* Reset players position and win state when player reach water or collide with
+     * enemy or rock item
+     */
+    function reset() {
+        player.x = getRandomInt(0,6)*100;
+        player.y = 563;
+        player.win = false;
+    }
+
+    /* This functio catch all collision in the game. It checks if
+     * - player collide with emeny
+     * - player collect some item
+     * - player collide with rock item
+     * - player collect heart item (recieve one more life)
+     */
+    function checkCollisions() {
+        /* Iterate through all enemies and check if enemy position and player position
+         * is the same
+         */
+        allEnemies.forEach(function(enemy) {
+            /* Check if enemy position and player position is the same
+             * If so:
+             * - fire players life (both in player object and in score object)
+             * - if number of players life is 0 then set flag that player lose the game
+             */
+            if(enemy.x - 85 < player.x &&
+               enemy.x + 85 > player.x &&
+               enemy.y - 50 < player.y &&
+               enemy.y + 50 > player.y) {
+                player.life -= 1;
+                score.score.life -= 1;
+                if (player.life === 0) {
+                    score.lose = true;
+                }
+                reset();
+            }
+        });
+
+        /* Check if player position and item position is the same
+         * If so:
+         * - update score for appropriate item
+         * - or reduce player life if item type is rock
+         * --- if number of players life is 0 then end the game
+         * - or increase player life if item type is heart
+         * --- the maximum number of life is 5, so it can't be more than 5
+         */
+        allItems.forEach(function(item) {
+            if(item.x - 85 < player.x &&
+               item.x + 85 > player.x &&
+               item.y - 50 < player.y &&
+               item.y + 50 > player.y) {
+                if (item.type == 'blueGem') {
+                    score.addScore('blueGem');
+                    item.collide = true;
+                } else if (item.type == 'orangeGem') {
+                    score.addScore('orangeGem');
+                    item.collide = true;
+                } else if (item.type == 'greenGem') {
+                    score.addScore('greenGem');
+                    item.collide = true;
+                } else if (item.type == 'key') {
+                    score.addScore('key');
+                    item.collide = true;
+                } else if (item.type == 'selector') {
+                    score.addScore('selector');
+                    item.collide = true;
+                } else if (item.type == 'star') {
+                    score.addScore('star');
+                    item.collide = true;
+                } else if (item.type == 'heart') {
+                    item.collide = true;
+                    player.life = player.life < 5 ? player.life + 1 : 5;
+                    score.score.life = score.score.life < 5 ? score.score.life + 1 : 5;
+                } else if (item.type == 'rock') {
+                    item.collide = true;
+                    player.life -= 1;
+                    score.score.life -= 1;
+                    if (player.life === 0) {
+                        score.lose = true;
+                    }
+                    reset();
+                }
+            }
+        });
+
+        /* If player reached water update score and reset players position to the start
+         */
+        if(player.win) {
+            score.addScore('waterReached');
+            reset();
+        }
+    }
+
+    /* Check if overall score is reached win state
+     * Win state is when
+     * - all items was collected not less than 3 times
+     * - and water was reached not less than 3 times
+     */
+    function Win() {
+            if (score.score.blueGem >= 3 &&
+                score.score.orangeGem >= 3 &&
+                score.score.greenGem >= 3 &&
+                score.score.key >= 3 &&
+                score.score.selector >= 3 &&
+                score.score.star >= 3 &&
+                score.score.waterReached >= 3) {
+            score.win = true;
+            player.win = true;
+        }
+    }
+    /* Go ahead and load all of the images we know we're going to need to
+     * draw our game level. Then set init as the callback method, so that when
+     * all of these images are properly loaded our game will start.
+     */
+    Resources.load([
+        'images/stone-block.png',
+        'images/water-block.png',
+        'images/grass-block.png',
+        'images/enemy-bug.png',
+        'images/char-boy.png',
+        'images/char-boy-small.png',
+        'images/char-cat-girl.png',
+        'images/char-cat-girl-small.png',
+        'images/char-horn-girl.png',
+        'images/char-horn-girl-small.png',
+        'images/char-pink-girl.png',
+        'images/char-pink-girl-small.png',
+        'images/char-princess-girl.png',
+        'images/char-princess-girl-small.png',
+        'images/gem-blue.png',
+        'images/gem-orange.png',
+        'images/gem-green.png',
+        'images/Heart.png',
+        'images/Key.png',
+        'images/Rock.png',
+        'images/Selector.png',
+        'images/Star.png',
+        'images/gem-blue-small.png',
+        'images/gem-orange-small.png',
+        'images/gem-green-small.png',
+        'images/Heart-small.png',
+        'images/Key-small.png',
+        'images/Selector-small.png',
+        'images/Star-small.png'
+    ]);
+    Resources.onReady(init);
+
+    /* Assign the canvas' context object to the global variable (the window
+     * object when run in a browser) so that developer's can use it more easily
+     * from within their app.js files.
+     */
+    global.ctx = ctx;
+})(this);
